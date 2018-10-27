9395a8835b8fc3a1c4494206d8ac58edf46c6c18
Abby Turner <ataralass@gmail.com>
2018-10-27 15:47:09 -0400

    Remove submodule and add ui folder content

---
+++ ui/init.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+node_modules/.bin/node-sass --include-path scss src/App.scss src/App.css && npm start
---
+++ ui/src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Keycloak from 'keycloak-js';
+
+const keycloakJson = {
+    "realm": "demo",
+    "clientId": "frontend-app",
+    "url": "http://localhost:8080/auth",
+    "ssl-required": "external",
+    "resource": "frontend-app",
+    "public-client": true
+};
+
+const keycloak = Keycloak(keycloakJson);
+
+const getKeycloak = () => {
+    return keycloak;
+};
+
+keycloak.init({onLoad: 'login-required'})
+    .then(authenticated => {
+        if (authenticated) {
+            ReactDOM.render(<App keycloak={getKeycloak()}/>, document.getElementById('root'));
+        } else {
+            keycloak.login();
+        }
+    });
