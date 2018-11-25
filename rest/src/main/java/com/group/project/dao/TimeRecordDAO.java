fa2fabc523d5caf79efe170e031920f5a9030d10
Justin <Justin.Weston@Gmail.com>
2018-11-25 18:22:43 -0500

    Updated with outline of additional DAO methods
    Will author corresponding service methods later.

--- rest/src/main/java/com/group/project/dao/TimeRecordDAO.java
+++ rest/src/main/java/com/group/project/dao/TimeRecordDAO.java
@@ -1,14 +1,16 @@
 package com.group.project.dao;
 
-
-import com.group.project.dto.User;
 import com.group.project.entity.TimeRecord;
 
 import java.util.List;
 
 public interface TimeRecordDAO {
 
-    List<TimeRecord> getTimeRecords(User user);
+    int getUserId(String username);
+    List<TimeRecord> getEmployeeTimeRecords(int userId);
+    List<TimeRecord> getManagerTimeRecords(int userId);
     String saveClockIn(String username);
+    boolean isValidClockIn(String username);
     String saveClockOut(String username);
+    boolean isValidClockOut(String username);
 }
--- rest/src/main/java/com/group/project/dao/TimeRecordDAOImpl.java
+++ rest/src/main/java/com/group/project/dao/TimeRecordDAOImpl.java
@@ -1,6 +1,5 @@
 package com.group.project.dao;
 
-import com.group.project.dto.User;
 import com.group.project.entity.TimeRecord;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,32 +15,55 @@ public class TimeRecordDAOImpl implements TimeRecordDAO {
     //private SessionFactory sessionFactory;
 
     @Override
-    public List<TimeRecord> getTimeRecords(User user) {
+    public List<TimeRecord> getEmployeeTimeRecords(int userId) {
 
         // get the current Hibernate session
         // Session currentSession = sessionFactory.getCurrentSession()
 
-        // Create query for either Manager or Employee based on user.getRole()
         // Query<TimeRecord> query = currentSession.createQuery("SQL or HQL query here", TimeRecord.class);
-        // Make sure query is written to get TimeActionRecord.time_action_record_id, EmployeesRecord.employee_username, clock_in, clock_out
-        // If a manager, order by employee_username then clock_in, then clock_out (last one might be overkill)
-        // If employee, order by clock_in then clock_out
+        // Order by clock_in time with newest first
 
-        // execute query and get result list
+        // executes query and gets result list
         // List<TimeRecord> timeRecords = query.getResultList();
+        // return timeRecords;
 
         return null;
+    }
+
+    @Override
+    public List<TimeRecord> getManagerTimeRecords(int userId) {
+
+        // get the current Hibernate session
+        // Session currentSession = sessionFactory.getCurrentSession()
+
+        // Query<TimeRecord> query = currentSession.createQuery("SQL or HQL query here", TimeRecord.class);
+        // If a manager, order by employee_username then by clock_in with newest entries first
+
+        // executes query and gets result list
+        // List<TimeRecord> timeRecords = query.getResultList();
+        // return timeRecords;
+
+        return null;
+    }
+
+    @Override
+    public int getUserId(String username) {
+
+        // return existing user ID or create new user and return new id
+
+        return 0;
     }
 
     @Override
     public String saveClockIn(String username) {
 
         // Get employee_record_id from the username
-        // Use this to create a new TimeRecord object and save to database.
-        // id and clock in time should be generated from the database
-        // Ensure clock out time is initially null
+        // isValidClockIn(username) should be done here or on service layer before call
+        // if valid, create a new TimeRecord object and save to database
+        // timerecord id  and clock in time should be generated from the database
+        // Clock out time is initially null
 
-        // return success String message;
+        // return success/failure String message;
 
         return null;
     }
@@ -51,11 +73,29 @@ public class TimeRecordDAOImpl implements TimeRecordDAO {
 
         // Get employee_record_id from the username
         // Use this to look up the newest TimeActionRecord (greatest id# for that user most likely)
-        // If clock out time != null, return String error message
-        // Else use current time for clock out
+        // isValidClockOut(username) should be done here or on service layer before call
+        // if valid, use current time for clockout
 
-        // Consider database constraint to make sure clockout time is after clock in time (mostly for edits in future)
+        // Consider database constraint to make sure clockout time is after clock in time (mostly useful for edits in future)
+
+        // Calculate hours worked and save to database field.
+        // What format is best here as we'll ultimately need UI to display (e.g. 8.50 hrs is 8 and a half hours)
 
         return null;
+    }
+
+    @Override
+    public boolean isValidClockIn(String username) {
+        // was last clock in > X hours (use case missed punch out)
+        // or does last clock in have a clock out time (use case multiple punch in/out per day)
+
+        return false;
+    }
+
+    @Override
+    public boolean isValidClockOut(String username) {
+        // does newest TimeActionRecord have no clockout info already and does it occur within X hours
+
+        return false;
     }
 }
