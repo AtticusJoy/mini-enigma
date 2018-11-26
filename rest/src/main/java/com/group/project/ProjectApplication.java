5518f35718f437f9a6f4bab3bc7a8307d9220e2b
petar.petrov <petar.petrov@sandc.com>
2018-11-25 21:15:33 -0500

    modified ProjectApplication and application.properties

--- rest/src/main/java/com/group/project/ProjectApplication.java
+++ rest/src/main/java/com/group/project/ProjectApplication.java
@@ -2,10 +2,12 @@ package com.group.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 //created by petar.petrov
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ProjectApplication {
 
     public static void main(String[] args) {
