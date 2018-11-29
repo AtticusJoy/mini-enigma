461e00deb3177cc8b724c04d2e75c0f7df255350
Justin <Justin.Weston@Gmail.com>
2018-11-29 18:44:22 -0500

    Added @Repository for clarification
    Not required with extends JpaRepository, but added for clarity

--- rest/src/main/java/com/group/project/repository/EmployeeRecordRepository.java
+++ rest/src/main/java/com/group/project/repository/EmployeeRecordRepository.java
@@ -2,7 +2,9 @@ package com.group.project.repository;
 
 import com.group.project.entity.EmployeeRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmployeeRecordRepository extends JpaRepository<EmployeeRecord, Integer> {
 
     EmployeeRecord findByUsername(String username);
--- rest/src/main/java/com/group/project/repository/TimeRecordRepository.java
+++ rest/src/main/java/com/group/project/repository/TimeRecordRepository.java
@@ -2,9 +2,11 @@ package com.group.project.repository;
 
 import com.group.project.entity.TimeRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface TimeRecordRepository extends JpaRepository<TimeRecord, Integer> {
 
     List<TimeRecord> findByEmployeeId(int employeeId);
