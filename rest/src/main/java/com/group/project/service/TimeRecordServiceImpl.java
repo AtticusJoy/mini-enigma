de79660188c0cf809d6f887a64c93f5ebef7e629
Justin <Justin.Weston@Gmail.com>
2018-11-29 18:42:57 -0500

    Added @Autowired to constructor
    Not required for a single constructor, but good practice in case a second one is added in later

--- rest/src/main/java/com/group/project/service/TimeRecordServiceImpl.java
+++ rest/src/main/java/com/group/project/service/TimeRecordServiceImpl.java
@@ -4,9 +4,9 @@ import com.group.project.entity.EmployeeRecord;
 import com.group.project.entity.TimeRecord;
 import com.group.project.repository.EmployeeRecordRepository;
 import com.group.project.repository.TimeRecordRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.sql.Time;
 import java.util.List;
 
 @Service
@@ -15,6 +15,7 @@ public class TimeRecordServiceImpl implements TimeRecordService {
     private TimeRecordRepository timeRecordRepository;
     private EmployeeRecordRepository employeeRecordRepository;
 
+    @Autowired
     public TimeRecordServiceImpl(TimeRecordRepository timeRecordRepository,
                                  EmployeeRecordRepository employeeRecordRepository) {
         this.timeRecordRepository = timeRecordRepository;
