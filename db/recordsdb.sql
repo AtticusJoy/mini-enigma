19d80a3602981483fd100911825e1229e40dff30
Shirin <shirin.mohajer@gmail.com>
2018-12-02 13:50:31 -0500

    added a new colunm to time_action_record for total worked houres-varchar325

--- db/recordsdb.sql
+++ db/recordsdb.sql
@@ -35,7 +35,7 @@ time_action_record_id int(186) auto_increment,
 employee_record_id int(11),
 clock_in_timestamp datetime DEFAULT current_timestamp,
 clock_out_timestamp datetime,
-hours_worked varchar(12),
+hours_worked varchar(325),
 PRIMARY KEY (`time_action_record_id`),
 FOREIGN KEY (employee_record_id) REFERENCES recordsdb.employees_record(employee_record_id)
 );
