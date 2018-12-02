3ea743f77b536bc5733ae7f7c270694185fafe43
petar.petrov <petar.petrov@sandc.com>
2018-12-02 15:23:07 -0500

    added plain text headers

--- ui/src/utils/AxiosClient.js
+++ ui/src/utils/AxiosClient.js
@@ -2,13 +2,15 @@ import axios from 'axios/index';
 
 export default class AxiosClient {
 
-    mainPath = 'http://localhost:7070';
+    mainPath = 'http://rest:7070';
 
     clockIn = (keycloak) => {
+        const config = {
+            headers: {'Content-Type': 'text/plain'}
+        };
         console.log(keycloak.idTokenParsed.preferred_username);
-        axios.post(this.mainPath + "/clockIn", {
-            username: keycloak.idTokenParsed.preferred_username
-        }).then(response => {
+        axios.post(this.mainPath + "/clockIn", keycloak.idTokenParsed.preferred_username, config
+        ).then(response => {
             alert(response.data)
         }).catch(error => {
             alert("Unexpected error: " + error.data)
@@ -16,9 +18,11 @@ export default class AxiosClient {
     };
 
     clockOut = (keycloak) => {
-        axios.post(this.mainPath + "/clockOut", {
-            username: keycloak.idTokenParsed.preferred_username
-        }).then(response => {
+        const config = {
+            headers: {'Content-Type': 'text/plain'}
+        };
+        axios.post(this.mainPath + "/clockOut", keycloak.idTokenParsed.preferred_username, config
+        ).then(response => {
             alert(response.data)
         }).catch(error => {
             alert("Unexpected error: " + error.data)
