b114c5cc0b8f3dfaaa3bf8ae75f0fbdf7181514c
Cody Penta <home@Codys-MBP.home>
2018-12-05 17:29:36 +0000

    Adding styling to the front end

--- ui/src/App.js
+++ ui/src/App.js
@@ -5,12 +5,19 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
 import DataTable from './components/Table';
 import AxiosClient from "./utils/AxiosClient";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import logo from "./Cronos_logo.png"
+// import Particles from "react-particles-js "; Get Docker to recognize this package?
 
 class App extends Component {
 
-    state = {
-        keycloak: this.props.keycloak
-    };
+    constructor(props) {
+        super(props)
+        this.state = {
+            keycloak: this.props.keycloak
+        }
+    }
 
     logout = () => {
         this.props.keycloak.logout();
@@ -28,14 +35,59 @@ class App extends Component {
     render() {
         return (
             <div>
-                <MuiThemeProvider>
-                    <RaisedButton label="Clock In" onClick={this.clockIn}/>
-                    <RaisedButton label="Clock Out" onClick={this.clockOut}/>
-                    <RaisedButton label="Logout" onClick={this.logout}/>
-                    <div>
-                        <DataTable keycloak={this.props.keycloak}/>
-                    </div>
-                </MuiThemeProvider>
+                <Typography variant="h3" color="primary" style={{display:"inline"}}>
+                    Chronos
+                    <img src={logo} alt="logo" width="50px"/>
+                </Typography>
+                <div style={{
+                    backgroundImage: 'linear-gradient(#add8e6, #00f',
+                    display: "flex",
+                    flexGrow: 1,
+                    justifyContent: "center",
+                    margin: "30px",
+                    paddingTop: "50px"
+                }}>
+                    {/* <Particles 
+                    params={{
+                            particles: {
+                                line_linked: {
+                                    shadow: {
+                                        enable: true,
+                                        color: "#3CA9D1",
+                                        blur: 5
+                                    }
+                                }
+                            }
+                        }}
+                    style={{
+                        width: '100%',
+
+                    }}
+                    /> */}
+                    <MuiThemeProvider>
+                        <Grid container column justify="center" spacing={32}>
+                            <Grid container spacing={8} justify="right" alignItems="center" style={{marginLeft:"30px"}}>
+                                <Grid item xs={3}>
+                                    <RaisedButton label="Clock In" onClick={this.clockIn}/>
+                                </Grid>
+                                <Grid item xs={3}>
+                                    <RaisedButton label="Clock Out" onClick={this.clockOut}/>
+                                </Grid>
+                                <Grid item xs={3} >
+                                    <span></span>
+                                </Grid>
+                                <Grid item xs={3}>
+                                    <RaisedButton label="Logout" onClick={this.logout}/>
+                                </Grid>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <div>
+                                <DataTable keycloak={this.props.keycloak}/>
+                                </div>
+                            </Grid>
+                        </Grid>
+                    </MuiThemeProvider>
+                </div>
             </div>
         );
     }
